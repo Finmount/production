7c2fa80fd811d8f094db31e8df0c83cb899647f5
Finmount <info@finmount.ie>
2026-06-14 19:22:55 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,6 +14,7 @@ import {
   Star
 } from 'lucide-react';
 import useScrollToTop from '../utils/useScrollToTop';
+import { motion } from "framer-motion";
 import TeamSection from '../components/TeamSection';
 
 interface HomePageProps {
